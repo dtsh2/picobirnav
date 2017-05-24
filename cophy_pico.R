898d1510e40474e5daea6f6622486e886b22b568
dtsh2 <davidtshayman@gmail.com>
2017-05-24 17:35:34 +1200

    first code with cophy

---
+++ cophy_pico.R
@@ -0,0 +1,19 @@
+rm(list = ls())
+library(phytools)
+library(ape) 
+
+tree1<-read.newick("Picobirnavirus complete.newick")
+tree2<-read.newick("Picobirnavirus short.newick")
+
+tt<-charmatch(tree1$tip.label,tree2$tip.label)
+
+tree1$tip.label<-LETTERS[1:length(tree1$tip.label)]
+tree2$tip.label<-LETTERS[tt]
+
+obj<-cophylo(tree1,tree2,rotate = F)
+plot(obj,cex=0.01)
+
+pdf("cophy_pico.pdf",width = 7,height = 7)
+obj<-cophylo(tree1,tree2,rotate = T)
+plot(obj,cex=0.01)
+dev.off()
