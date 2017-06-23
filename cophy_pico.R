21b3bae408a9075df573464a73e65ee13a898525
dtsh2 <davidtshayman@gmail.com>
2017-06-23 17:21:07 +1200

    New data and trees

--- cophy_pico.R
+++ cophy_pico.R
@@ -34,3 +34,92 @@ obj<-cophylo(tree1,tree2,rotate = T)
 plot(obj,cex=0.01)
 dev.off()
 
+##
+
+tree1<-read.newick("ML tree of NucleotideAlignmentTrimmedSHORT.newick")
+tree2<-read.newick("ML tree of NucleotideAlignmentTrimmed.newick")
+
+tt<-charmatch(tree1$tip.label,tree2$tip.label)
+
+x<-tree1$tip.label
+x1 = sapply(strsplit(x, split='_', fixed=TRUE),function(x) (x[1]))
+tree1$tip.label<-x1
+
+x<-tree2$tip.label
+x1 = sapply(strsplit(x, split='_', fixed=TRUE),function(x) (x[1]))
+tree2$tip.label<-x1
+
+obj<-cophylo(tree1,tree2,rotate = F)
+plot(obj,cex=0.01)
+
+pdf("cophy_pico_NA_TrimAI.pdf",width = 7,height = 7)
+obj<-cophylo(tree1,tree2,rotate = T)
+plot(obj,cex=0.01)
+dev.off()
+
+#### protein
+
+tree1<-read.newick("ML tree of ProteinAlignmentTrimmedSHORT.newick")
+tree2<-read.newick("ML tree of ProteinAlignmentTrimmed.newick")
+
+tt<-charmatch(tree1$tip.label,tree2$tip.label)
+
+x<-tree1$tip.label
+x1 = sapply(strsplit(x, split='_', fixed=TRUE),function(x) (x[1]))
+tree1$tip.label<-x1
+
+x<-tree2$tip.label
+x1 = sapply(strsplit(x, split='_', fixed=TRUE),function(x) (x[1]))
+tree2$tip.label<-x1
+
+obj<-cophylo(tree1,tree2,rotate = F)
+plot(obj,cex=0.01)
+
+pdf("cophy_pico_Prot_TrimAI.pdf",width = 7,height = 7)
+obj<-cophylo(tree1,tree2,rotate = T)
+plot(obj,cex=0.01)
+dev.off()
+
+#### trimmed
+
+tree1<-read.newick("ML tree of ProteinAlignmentTrimmedSHORT.newick")
+tree2<-read.newick("ML tree of NucleotideAlignmentTrimmedSHORT.newick")
+
+tt<-charmatch(tree1$tip.label,tree2$tip.label)
+
+x<-tree1$tip.label
+x1 = sapply(strsplit(x, split='_', fixed=TRUE),function(x) (x[1]))
+tree1$tip.label<-x1
+
+x<-tree2$tip.label
+x1 = sapply(strsplit(x, split='_', fixed=TRUE),function(x) (x[1]))
+tree2$tip.label<-x1
+
+obj<-cophylo(tree1,tree2,rotate = F)
+plot(obj,cex=0.01)
+
+pdf("cophy_pico_TRIMMED_SHORT_TrimAI.pdf",width = 7,height = 7)
+obj<-cophylo(tree1,tree2,rotate = T)
+plot(obj,cex=0.01)
+dev.off()
+
+tree1<-read.newick("ML tree of ProteinAlignmentTrimmed.newick")
+tree2<-read.newick("ML tree of NucleotideAlignmentTrimmed.newick")
+
+tt<-charmatch(tree1$tip.label,tree2$tip.label)
+
+x<-tree1$tip.label
+x1 = sapply(strsplit(x, split='_', fixed=TRUE),function(x) (x[1]))
+tree1$tip.label<-x1
+
+x<-tree2$tip.label
+x1 = sapply(strsplit(x, split='_', fixed=TRUE),function(x) (x[1]))
+tree2$tip.label<-x1
+
+obj<-cophylo(tree1,tree2,rotate = F)
+plot(obj,cex=0.01)
+
+pdf("cophy_pico_TRIMMED_TrimAI.pdf",width = 7,height = 7)
+obj<-cophylo(tree1,tree2,rotate = T)
+plot(obj,cex=0.01)
+dev.off()
